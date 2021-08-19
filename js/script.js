c557019c0fa3bb165d1d1fb7dbfbd5f3fe1f9f2f
Evgeniy Grabko <djek.grabko@gmail.com>
2021-08-19 12:56:37 +0300

    end lesson 6

--- js/script.js
+++ js/script.js
@@ -7,5 +7,17 @@ $(function(){
         $('nav ul').resize();
         }
     });
+
+    $('.service-slider').slick({
+        arrows:false,
+        dots:true,
+        responsive:[{
+            breakpoint:600,
+            settings:{
+                dots:false,
+                centerMode:true
+            }
+        }]
+    });
 });
 
